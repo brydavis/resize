3e88831a4f799b224d82d9d1c5911579a4c31d3a
Bryan Davis <bryan@qnm.io>
2017-07-21 14:45:37 -0700

    Update resize.go

--- resize.go
+++ resize.go
@@ -1,9 +1,10 @@
-package resize
+package main
 
 import (
 	"errors"
 	"fmt"
 	"image"
+	// "image/color"
 	"image/jpeg"
 	"image/png"
 	"io"
@@ -11,6 +12,48 @@ import (
 	"os"
 	"path"
 )
+
+var digits = ""
+
+	// file, _ := os.Open("un_secretary.jpg")
+	// img, _ := ResizeColors(file, 1500, 1500)
+	// err := WriteToFile("new_example.jpg", img)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+
+func ResizeColors(reader io.Reader, width, height int) (*image.Gray, error) {
+	img, _, err := image.Decode(reader)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	dx := float64(width) / float64(img.Bounds().Max.X)
+	dy := float64(height) / float64(img.Bounds().Max.Y)
+
+	rect := image.Rect(0, 0, width, height)
+	// i := image.NewRGBA(rect)
+	i := image.NewGray(rect)
+
+	for iy := 0; iy <= rect.Max.Y; iy += 1 {
+		for ix := 0; ix <= rect.Max.X; ix += 1 {
+			pixel := img.At(int(float64(ix)/dx), int(float64(iy)/dy))
+			red, green, blue, _ := pixel.RGBA()
+			gray := int((red+blue+green)/3) / 100
+
+			digits += fmt.Sprintf("%d", gray)
+
+			i.Set(ix, iy, pixel)
+		}
+		digits += "\n"
+
+	}
+
+	ioutil.WriteFile("digits.txt", []byte(digits), 0777)
+
+	return i, nil
+}
 
 func ResizePixels(reader io.Reader, width, height int) (*image.RGBA, error) {
 	img, _, err := image.Decode(reader)
@@ -33,59 +76,59 @@ func ResizePixels(reader io.Reader, width, height int) (*image.RGBA, error) {
 	return i, nil
 }
 
-func ResizeFolderPercent(dir string, percent float64, prefix string) {
-	d, _ := ioutil.ReadDir(dir)
-	for _, item := range d {
-		filename := item.Name()
-		file, _ := os.Open(filename)
-		defer file.Close()
+// func ResizeFolderPercent(dir string, percent float64, prefix string) {
+// 	d, _ := ioutil.ReadDir(dir)
+// 	for _, item := range d {
+// 		filename := item.Name()
+// 		file, _ := os.Open(filename)
+// 		defer file.Close()
 
-		ext := path.Ext(filename)
+// 		ext := path.Ext(filename)
 
-		if ext != ".jpg" && ext != ".jpeg" && ext != ".png" {
-			fmt.Printf("File not an image: %s\n", filename)
-		}
+// 		if ext != ".jpg" && ext != ".jpeg" && ext != ".png" {
+// 			fmt.Printf("File not an image: %s\n", filename)
+// 		}
 
-		img, err := ResizePercent(file, percent)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			new_filename := fmt.Sprintf("%s_%s", prefix, filename)
-			if err := WriteToFile(new_filename, img); err != nil {
-				fmt.Println(err)
-			}
-		}
-	}
-}
+// 		img, err := ResizePercent(file, percent)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		} else {
+// 			new_filename := fmt.Sprintf("%s_%s", prefix, filename)
+// 			if err := WriteToFile(new_filename, img); err != nil {
+// 				fmt.Println(err)
+// 			}
+// 		}
+// 	}
+// }
 
-func ResizeFolderPixels(dir string, width, height int, prefix string) {
-	d, _ := ioutil.ReadDir(dir)
-	for _, item := range d {
-		filename := item.Name()
-		file, _ := os.Open(filename)
-		defer file.Close()
+// func ResizeFolderPixels(dir string, width, height int, prefix string) {
+// 	d, _ := ioutil.ReadDir(dir)
+// 	for _, item := range d {
+// 		filename := item.Name()
+// 		file, _ := os.Open(filename)
+// 		defer file.Close()
 
-		ext := path.Ext(filename)
+// 		ext := path.Ext(filename)
 
-		if ext != ".jpg" && ext != ".jpeg" && ext != ".png" {
-			fmt.Printf("File not an image: %s\n", filename)
-		} else {
-			img, err := ResizePixels(file, width, height)
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				new_filename := fmt.Sprintf("%s_%s", prefix, filename)
-				if err := WriteToFile(new_filename, img); err != nil {
-					fmt.Println(err)
-				}
-			}
-		}
+// 		if ext != ".jpg" && ext != ".jpeg" && ext != ".png" {
+// 			fmt.Printf("File not an image: %s\n", filename)
+// 		} else {
+// 			img, err := ResizePixels(file, width, height)
+// 			if err != nil {
+// 				fmt.Println(err)
+// 			} else {
+// 				new_filename := fmt.Sprintf("%s_%s", prefix, filename)
+// 				if err := WriteToFile(new_filename, img); err != nil {
+// 					fmt.Println(err)
+// 				}
+// 			}
+// 		}
 
-	}
+// 	}
 
-}
+// }
 
-func WriteToFile(filename string, img *image.RGBA) error {
+func WriteToFile(filename string, img *image.Gray) error {
 	out, _ := os.Create(filename)
 
 	switch ext := path.Ext(filename); ext {
